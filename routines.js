ac20f57002f2193f50056ecea17f37945643e34d
Mohammed Nihal Ansari <nihal.ansari@in.ibm.com>
2021-03-01 01:39:11 +0800

    improved

--- routines.js
+++ routines.js
@@ -1,18 +1,13 @@
 const inventoryDataset = "./data/inventory.json";
-const locationDataset = "./data/location.json";
 const getStockInfo = (product,suburb) => {
     const fs = require('fs');
     let fileAsStr = fs.readFileSync(inventoryDataset).toString();
     let fileAsJson = JSON.parse(fileAsStr);
-    /*
-    const coord = suburbExists();
-    if(coord === null){
-        return "Error! Store/Suburb does not exist."
-    }*/
+
     //Step1: identify unique courses. Append all student names against the subject_code
     for(i of fileAsJson){
         console.log(i);
-        //trimming to be safe, as I have noticed whitespace suffixed from dialogflow
+        //trimming to be safe, as I have noticed whitespace suffixed to parameter values (from dialogflow)
         product=product.toString().trim();
         if (i.productname == product){
             for (p of i.productdetails.storesinventory){
@@ -25,21 +20,10 @@ const getStockInfo = (product,suburb) => {
         }
 
     }
-    return 0
+    return 0;
 
 }
 
-const suburbExists = function(suburb){
-    const fs = require('fs');
-    let fileAsStr = fs.readFileSync(locationDataset).toString();
-    var fileAsJson = JSON.parse(fileAsStr);
-    for(i of fileAsJson){
-        if (i.suburb == suburb ){
-            return fileAsJson.gps_coordinates;
-        }
-    }
-    return null;
-}
 
 exports.getStockInfo = getStockInfo;
 
